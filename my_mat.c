36fc6a021d53024364f6bb720a7d439a4b32fcd3
Sibo523 <121120413+Sibo523@users.noreply.github.com>
2024-02-09 02:44:41 +0200

    Update my_mat.c
    no need for the second for מקונן

--- my_mat.c
+++ my_mat.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "my_mat.h"
 
-#define INFINITE  9999999
-#define MATRIX_SIZE  10
-
+#define INFINITE  9999999 // we suppose that there's no edge's bigger than this number
+#define MATRIX_SIZE  10 
+// we get the input and intialize the matrix 
 void getMatVal(int matrix[MATRIX_SIZE][MATRIX_SIZE]) {
     for (int row =  0; row < MATRIX_SIZE; row++) {
         for (int col =  0; col < MATRIX_SIZE; col++) {
@@ -11,25 +11,28 @@ void getMatVal(int matrix[MATRIX_SIZE][MATRIX_SIZE]) {
         }
     }
 }
-int helperMatrix[MATRIX_SIZE][MATRIX_SIZE]; // this is a global matrix which I know to be 
+// this is a global matrix which I know to be of Matrix_size
+int helperMatrix[MATRIX_SIZE][MATRIX_SIZE]; 
 
-int isTherePath(int matrix[MATRIX_SIZE][MATRIX_SIZE], int startIndex, int endIndex) {
-    
+int isTherePath(int matrix[MATRIX_SIZE][MATRIX_SIZE], int startIndex, int endIndex) {    
     // Copy the original matrix to the helper matrix
     for (int row =  0; row < MATRIX_SIZE; row++) {
         for (int col =  0; col < MATRIX_SIZE; col++) {
+            if (row != col && matrix[row][col] ==  0) {
+                helperMatrix[row][col] = INFINITE
+            } 
             helperMatrix[row][col] = matrix[row][col];
         }
     }
     
     // Initialize the helper matrix with infinite values where the original has zero
-    for (int row =  0; row < MATRIX_SIZE; row++) {
-        for (int col =  0; col < MATRIX_SIZE; col++) {
-            if (row != col && helperMatrix[row][col] ==  0) {
-                helperMatrix[row][col] = INFINITE;
-            }
-        }
-    }
+    // for (int row =  0; row < MATRIX_SIZE; row++) {
+    //     for (int col =  0; col < MATRIX_SIZE; col++) {
+    //         if (row != col && helperMatrix[row][col] ==  0) {
+    //             helperMatrix[row][col] = INFINITE;
+    //         }
+    //     }
+    // }
     
     // Apply Floyd-Warshall algorithm to find shortest paths
     for (int k =  0; k < MATRIX_SIZE; k++) {
